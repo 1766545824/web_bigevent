bb8a057dbef48aca7420e2b10e26d53b492a0059
cjl <1766545824@qq.com>
2021-11-22 22:24:03 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,28 @@
 // 在这个函数中 可以拿到我们给Ajax 提供的配置对象
 $.ajaxPrefilter(function (options) {
   options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
-  console.log(options.url);
+  // console.log(options.url);
+
+  // 统一为有权限的接口，设置 headers 请求头
+  // 判断是否有 /my 需要权限的接口才加这个
+  if (options.url.indexOf('/my') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  // 全局统一挂在 complete 回调函数
+  options.complete = function (res) {
+    // 不论成功还是失败 最终都会调用 complete 回调函数
+    // console.log('执行力complete回调');
+    // console.log(res);
+    // 在 complete 回调函数中 可以使用 res.responseJSON
+    // 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 1.强制清空 token
+      localStorage.removeItem('token');
+      // 2.强制跳转到登录页
+      location.href = 'login.html';
+    }
+  }
 });
